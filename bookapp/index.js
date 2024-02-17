c46e3b35d2d08ce99a84420ea5cfbc199a4b9fa0
Andrey Tikhonenkov <ollatair@gmail.com>
2024-02-17 20:24:25 +0300

    добавлены страницы login register profile

--- bookapp/index.js
+++ bookapp/index.js
@@ -1,24 +1,48 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
+const session = require('express-session')
+const passport = require('passport')
+const LocalStrategy = require('passport-local').Strategy
+
 const indexRouter = require('./routes/index');
 const bookRouter = require('./routes/book');
+const userRouter = require('./routes/user');
 
 const user = require('./routes/api/user');
 const book = require('./routes/api/book');
 const error404 = require('./middleware/err-404');
 
 const bookController = require('./controllers/booksPages');
+const userController = require('./controllers/userPages');
 
 const PORT = process.env.PORT || 3000; 
 const DB_URL = process.env.DB_URL || "localhost";
 
+
+const options = {
+    usernameField: "username",
+    passwordField: "password",
+  }
+  
+  passport.use('local', new LocalStrategy(options, userController.verifyUser))
+  
+  passport.serializeUser(userController.serializeUser)
+  passport.deserializeUser(userController.serializeUser)
+ 
+
 const app = express();
-app.use(express.urlencoded());
 app.set('view engine', 'ejs');
+
+app.use(express.urlencoded());
+app.use(session({ secret: 'SECRET'}));
+
+app.use(passport.initialize())
+app.use(passport.session())
 
 app.use('/', indexRouter);
 app.use('/books', bookRouter);
+app.use('/user', userRouter);
 app.use('/public', express.static(`${__dirname}/public`));
 app.use('/api/user', user);
 app.use('/api/books', book);
@@ -27,8 +51,7 @@ app.use(error404);
 
 
 async function start() {
-    try {
-        console.log(DB_URL);
+    try { 
     await mongoose.connect(DB_URL, {
         useNewUrlParser: true,
     });
--- bookapp/routes/api/user.js
+++ bookapp/routes/api/user.js
@@ -2,9 +2,9 @@ const express = require('express');
 
 const router = express.Router();
 
-router.post('/login', (req, res) => {
-  res.status(200);
-  res.json({ id: 1, mail: 'test@mail.ru' });
-});
+// router.post('/login', (req, res) => {
+//   res.status(200);
+//   res.json({ id: 1, mail: 'test@mail.ru' });
+// });
 
 module.exports = router;
